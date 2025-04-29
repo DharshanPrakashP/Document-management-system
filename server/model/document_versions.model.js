a8f33cbc2e1804384ed5f8d0dd009748ebb18816
Balamurali B <bbalamurali2004@gmail.com>
2025-04-29 21:56:34 +0530

    categories and document version models added

---
+++ server/model/document_versions.model.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const documentVersionSchema = new mongoose.Schema({
+    id: {
+        type: Number,
+        required: true
+    },
+    documentId: Number,
+    version_number: Number,
+    file_url: String,
+    notes: String,
+    created_at: { 
+        type: Date,
+        default: Date.now
+    }
+});
+
+const DocumentVersionModel = mongoose.model('DocumentVersion', documentVersionSchema);
+
+module.exports = DocumentVersionModel;
