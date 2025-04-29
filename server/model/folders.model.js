27aee70ec2956712f46abf6fbe6940a5a9af50df
DineshR0066 <ramdinesh2709@gmail.com>
2025-04-29 21:35:15 +0530

    folder model and the user model are updated by adding schema

---
+++ server/model/folders.model.js
@@ -0,0 +1,18 @@
+const mongoose = require ('mongoose');
+
+const folderSchema = new mongoose.Schema({
+    name: {
+        type :String,
+        required : true,
+    },
+    description: String,
+    user_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    created_at: { 
+        type: Date, 
+        default: Date.now
+     }
+  });
+  
+  const FolderModel = mongoose.model('Folder', folderSchema);
+
+  module.exports = FolderModel;
